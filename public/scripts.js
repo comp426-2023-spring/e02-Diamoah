d09e0b5e8e558b25229ebfa1622672f660a3ee50
Diamoah Ngwayah <diamoah@gmail.com>
2023-05-09 10:38:04 -0400

    Init

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,140 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+var opponent = false;
+const moves = ["rock", "paper", "scissors", "lizard", "spock"];
+var rps = true;
+var move = "rock";
+var rps_endpoint = "/app/rps/play/";
+var rpsls_endpoint = "/app/rpsls/play/";
+ 
+function playgamefromrpsorrpsls(index) {
+    move = moves[index];
+}
+
+function firstsubmit(event) {
+    event.preventDefault();
+    if (opponent) {
+        
+        document.getElementById("decision").hidden = true;
+        document.getElementById("rps-input").hidden = false;
+        if (rps) {
+            document.getElementById("lizard-label").hidden = true;
+            document.getElementById("spock-label").hidden = true;
+            document.getElementById("lizard").hidden = true;
+            document.getElementById("spock").hidden = true;    
+        }
+        
+
+    } else { 
+        
+        if (rps) { 
+            var random_move = Math.floor(Math.random() * 3);
+            random_move = moves[random_move];
+            var api_call = rps_endpoint + random_move;
+            fetch(api_call).then(response => response.json()).then(data => {
+                document.getElementById("results").innerText = JSON.stringify(data);
+                document.getElementById("decision").hidden = true;
+                document.getElementById("results").hidden = false;
+            });
+        } else {
+            
+            var random_move = Math.floor(Math.random() * 5);
+            random_move = moves[random_move];
+            var api_call = rpsls_endpoint + random_move;
+            fetch(api_call).then(response => response.json()).then(data => {
+                document.getElementById("results").innerText = JSON.stringify(data);
+                document.getElementById("decision").hidden = true;
+                document.getElementById("results").hidden = false;
+            });
+        }       
+    }
+}
+
+
+function secondsubmit(event) {
+    event.preventDefault();
+    document.getElementById("lizard-label").hidden = false;
+    document.getElementById("spock-label").hidden = false;
+    document.getElementById("lizard").hidden = false;
+    document.getElementById("spock").hidden = false;
+    document.getElementById("rps-input").hidden = true;
+
+    if (rps) {
+        var api_call = rps_endpoint + move;
+        fetch(api_call).then(response => response.json()).then(data => {
+            document.getElementById("results").innerText = JSON.stringify(data);
+            document.getElementById("decision").hidden = true;
+            document.getElementById("results").hidden = false;
+        });
+    } else {
+        var api_call = rpsls_endpoint + move;
+        fetch(api_call).then(response => response.json()).then(data => {
+            document.getElementById("results").innerText = JSON.stringify(data);
+            document.getElementById("decision").hidden = true;
+            document.getElementById("results").hidden = false;
+        });
+    }  
+}
+
+
+
+function playingrps() {
+    rps = true;
+}
+
+function playingrpsls() {
+    rps = false;
+}
+
+
+function playingopponent() {
+    opponent = !opponent;
+}
+
+
+function viewrules(event) {
+    event.preventDefault();
+    document.getElementById("rules").innerText =
+    `Rules for Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock CRUSHES Scissors
+    
+    Rules for the Lizard-Spock Expansion of Rock Paper Scissors:
+
+    - Scissors CUTS Paper
+    - Paper COVERS Rock
+    - Rock SMOOSHES Lizard
+    - Lizard POISONS Spock
+    - Spock SMASHES Scissors
+    - Scissors DECAPITATES Lizard
+    - Lizard EATS Paper
+    - Paper DISPROVES Spock
+    - Spock VAPORIZES Rock
+    - Rock CRUSHES Scissors`;
+    document.getElementById("rules-button").hidden = true;
+    document.getElementById("rules").hidden = false;
+    document.getElementById("hide-rules-button").hidden = false;
+}
+
+function hiderules() {
+    
+    document.getElementById("rules").hidden = true;
+    document.getElementById("hide-rules-button").hidden = true;
+    document.getElementById("rules-button").hidden = false;
+}
+
+function startOver() {
+    rps = true;
+    opponent = false;
+    move = "rock";
+    document.getElementById("rules-form").reset();
+    document.getElementById("results").innerHTML = "";
+    document.getElementById("rps-input").reset();
+    document.getElementById("decision").reset();
+    document.getElementById("results").hidden = true;
+    document.getElementById("rps-input").hidden = true;
+    document.getElementById("decision").hidden = false;
+}
